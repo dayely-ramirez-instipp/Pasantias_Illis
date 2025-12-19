7567baa13daf5e4ee03af00b7518ac35fa8c5900
iker-aviles-instipp <iaviles226_est@instipp.edu.ec>
2025-12-19 16:50:49 -0500

    actualizacion

--- Proyecto_illis/settings.py
+++ Proyecto_illis/settings.py
@@ -36,9 +36,9 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # Asegúrate también de que estas opciones estén así para producción
-CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
-
+CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # DEBUG = True
 
