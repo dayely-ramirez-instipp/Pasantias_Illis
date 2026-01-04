2bb954bdecc18688d919704694865dac4c631738
iker-aviles-instipp <iaviles226_est@instipp.edu.ec>
2026-01-04 09:32:09 -0500

    Update settings.py

--- Proyecto_illis/settings.py
+++ Proyecto_illis/settings.py
@@ -160,8 +160,10 @@ if DEBUG:
     STATICFILES_DIRS = [
         BASE_DIR / "static",
     ]
-    
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # --------------------------------------------------
 # DEFAULT PK
